021c6677c3edbb0728d1dfdefe956253590eb0a8
siefca <siefca>
2003-11-28 11:13:15 +0000

    - testing program modified a bit - added script for moving known files into a new dir

--- src/test.c
+++ src/test.c
@@ -5,4 +5,20 @@
 
 int main (int argc, char *argv[])
 {
+    int x;
+    const char *a[] = { ".muttrc", "asdfasdf", "/home/.muttrc",
+			"/home/users/siefca", "/home/users/siefca/", "/home/users/siefca/.muttrc",
+			"/home/users/siefca.muttrc", "/", "", "/home/users/siefca/.muttrc",
+			"/home/users/siefca/a/b", "/home/users/siefca/a/b/", "home/users/siefca/a",
+			"a/b" };
+    
+    printf("\nHOME_ETC:\t%s\n\n", get_home_etc_core(1));
+
+    for (x = 0; x < sizeof(a) / sizeof(char *); x++)
+    {
+	printf(" path:\t\t%s\n result:\t%s\n----------------------------\n",
+	        a[x],
+		home_etc_path_core(a[x]));
+    }
+    return 0;
 }
--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "home_etc.h"
-
-int main(int argc, char *argv[])
-{
-    printf ("katalog HOME_ETC: %s\n", get_home_etc(1));
-    
-    return 0;
-}
